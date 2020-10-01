41d7fdc6efa80f85fdb8115907772294d17b1e35
corewithharry <72163903+corewithharry@users.noreply.github.com>
2020-10-01 20:12:07 +0530

    Updated doc's

--- frameworks/cocos2d-html5/cocos2d/audio/CCAudio.js
+++ frameworks/cocos2d-html5/cocos2d/audio/CCAudio.js
@@ -1,12 +1,12 @@
 /****************************************************************************
- Copyright (c) 2008-2010 Ricardo Quesada
- Copyright (c) 2011-2012 cocos2d-x.org
+ Copyright (c) 2008-2010 Deepak Kataria
+ Copyright (c) 2011-2012 cocos2d-x.nic
  Copyright (c) 2013-2014 Chukong Technologies Inc.
 
  http://www.cocos2d-x.org
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
+ Permission is hereby granted, free of charges, to any person obtaining a copy
+ of this software and associated documentation file (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
